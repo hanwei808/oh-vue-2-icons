12cd6d234a5ed316a3f19fd900e1d9122c92d343
hanwei <hancenter808@outlook.com>
2024-04-23 17:30:50 +0800

    update: 增加打包代码压缩

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,12 +1,20 @@
 import analyze from "rollup-plugin-analyzer";
 import postcss from "rollup-plugin-postcss";
 import esbuild from "rollup-plugin-esbuild";
+import { terser } from 'rollup-plugin-terser';
 
 const base = {
   input: "src/index.js",
   plugins: [
     esbuild(),
     postcss(),
+    terser({
+      mangle: true,
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }),
     analyze({ summaryOnly: true, hideDeps: true })
   ]
 };
